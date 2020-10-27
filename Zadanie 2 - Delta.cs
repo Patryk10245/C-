669a60841967a250ba67990f5e4e93f007b5c707
Patryk10245 <59138469+Patryk10245@users.noreply.github.com>
2020-10-27 19:45:21 +0100

    Add files via upload

---
+++ Zadanie 2 - Delta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, delta, x1, x2;
+            Console.WriteLine("Program oblicza deltę");
+            Console.WriteLine("Podaj a ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj b ");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj c ");
+            c = double.Parse(Console.ReadLine());
+
+            delta = (b * b) - (4 * a * c);
+            Console.WriteLine("Delta wynosi: ");
+            Console.WriteLine(delta);
+
+            Console.ReadKey();
+        }
+    }
+}
