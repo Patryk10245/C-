04b5b8ce8e4bf112c1db84bf955b693ba0a3bd84
Patryk10245 <59138469+Patryk10245@users.noreply.github.com>
2020-10-27 19:47:34 +0100

    Add files via upload

---
+++ Zadanie 3 - BMI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double waga, wzrost, bmi;
+            Console.WriteLine("Podaj swoją wagę w kilogramach. ");
+            waga = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj swój wzrost w metrach. ");
+            wzrost = double.Parse(Console.ReadLine());
+            bmi = waga / (wzrost * wzrost);
+            Console.WriteLine("Twoje BMI wynosi: ");
+            Console.WriteLine(bmi);
+
+            Console.ReadKey();
+
+        }
+    }
+}
