a1561129af495426a3ea25f17171f67dae997c93
Patryk10245 <59138469+Patryk10245@users.noreply.github.com>
2020-10-27 19:44:02 +0100

    Add files via upload

---
+++ Zadanie 1 - Stopnie F.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double C, F;
+            Console.WriteLine("Podaj temperature w stopniach Celsjusza");
+            C = double.Parse(Console.ReadLine());
+            F = (C * 9 / 5) + 32;
+            Console.WriteLine("Temperatura w stopniach Fahrenheita wynosi: ");
+            Console.WriteLine(F);
+            Console.ReadKey();
+        }
+    }
+}
