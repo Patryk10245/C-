3d9dd3584b4c067f200e060ac6569a31cad92b46
Patryk10245 <59138469+Patryk10245@users.noreply.github.com>
2020-11-28 09:46:47 +0100

    Add files via upload

---
+++ Kolokwium Zadanie 3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolokwium_Zadanie_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wprowadz ilosc liczb ktore chcesz wprowadzic:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] table = new int[n];
+            Console.WriteLine("Wprowadz liczby:");
+            for (int i = 0; i < n; i++)
+            {
+                table[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int iloscParzystych = 0;
+            int iloscNieparzystych = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (table[i] % 2 == 0) iloscParzystych++;
+
+                else iloscNieparzystych++;
+            }
+            Console.WriteLine("Ilosc parzystych elementów" + iloscParzystych);
+            Console.WriteLine("Ilosc nieparzystych elementow: " + iloscNieparzystych);
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Kolokwium zadanie 4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolokwium_zadanie_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj słowo do analizy");
+            string x = Console.ReadLine();
+            while (x.Length > 0)
+            {
+                Console.Write(x[0] + " = ");
+                int y = 0;
+                for (int i = 0; i < x.Length; i++)
+                {
+                    if(x[0] == x[i])
+                    {
+                        y++;
+                    }
+                }
+                Console.WriteLine(y);
+                x = x.Replace(x[0].ToString(), string.Empty);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
